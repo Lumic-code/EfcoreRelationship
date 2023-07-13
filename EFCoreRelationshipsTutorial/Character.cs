a3272726507666bdf5876fc1109e89b0b2c05c23
Lumic-code <wisdomlumic0368@gmail.com>
2023-07-13 17:25:10 +0100

    Entity framework core relationships

---
+++ EFCoreRelationshipsTutorial/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EFCoreRelationshipsTutorial
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RpgClass { get; set; } = "Knight";
+
+        //One-to-many relationship
+        //JsonIgnore was used to ignore the object User.
+        [JsonIgnore]
+        public User User { get; set; }
+        public int UserId { get; set; }
+
+        //One-to-one relationship
+        public Weapon Weapon { get; set; }
+
+        //many-to-many relationship
+        public List<Skill> Skills { get; set; }
+       
+    }
+}
---
+++ EFCoreRelationshipsTutorial/User.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreRelationshipsTutorial
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+
+        //One-to-many relationship
+        public List<Character> Characters { get; set; }
+    }
+}
